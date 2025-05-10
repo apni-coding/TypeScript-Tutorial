7a4e121ddfac4daf8980d22e2996cddd8586f713
apni-coding <apnicoding72@gmail.com>
2025-05-10 14:57:12 +0530

    first commit

---
+++ number.js
@@ -0,0 +1,14 @@
+// keyword varName : dataType = value
+var a = 10.02;
+var b = 20;
+var d = 30;
+var hex = 0x7b;
+var binary = 121;
+var octal = 123;
+var scientific = 1.2e2;
+var e = -Infinity;
+console.log(e);
+var c = a + b + d;
+console.log(c);
+var f = 40;
+// f = "ac"
---
+++ number.ts
@@ -0,0 +1,19 @@
+// keyword varName : dataType = value
+var a : number = 10.02;
+var b : number = 20;
+var d = 30
+
+var hex : number = 0x7b;
+var binary : number = 0b1111001;
+var octal : number = 0o173;
+var scientific : number = 1.2e2;
+
+var e : number = -Infinity;
+console.log(e)
+
+
+var c : number = a +b +d ;
+console.log(c)
+
+var f = 40
+// f = "ac"
