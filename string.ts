30882b12ddc7904fc2a34ca0caffc5be78523f28
apni-coding <apnicoding72@gmail.com>
2025-05-10 15:51:26 +0530

    string and boolean data types

---
+++ string.ts
@@ -0,0 +1,15 @@
+var str: string = "vivek";
+var str1: string = "vivek";
+var str2: string = `verma`;
+
+var myName: string = "Vivek Verma";
+var age: number = 22;
+
+var myFullInfo: string = `My name is ${myName} age is ${age}`;
+
+var strAge: string = "" + age; //age.toString()
+console.log(strAge);
+console.log(age);
+
+var bool: boolean = Boolean(0); // False
+console.log(bool);
