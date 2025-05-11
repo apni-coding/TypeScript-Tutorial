d7f6d6981a70473e81a51fb8f52daa4755b1ed2e
apni-coding <apnicoding72@gmail.com>
2025-05-11 09:38:58 +0530

    array data types in typescript

---
+++ array.js
@@ -0,0 +1,15 @@
+// let fruits = ["apple", "oranage", false]
+// 1. using square brackets:-
+// let fruits : string[] = ["apple", "Orange"]
+//2. using generic array type
+var fruits = ["apple"];
+// Array declaration
+var num;
+// Array initialization
+num = [4];
+var arr = ["abc", 20, "def", true];
+var arr1 = [40, "abc"];
+console.log(arr[0]);
+for (var index in arr) {
+    console.log(arr[index]);
+}
