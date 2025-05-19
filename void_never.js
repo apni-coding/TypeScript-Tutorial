fd355dc103f626ba38439fca407b88edc399dafc
apni-coding <apnicoding72@gmail.com>
2025-05-19 18:38:30 +0530

    void and never data types in typescript

---
+++ void_never.js
@@ -0,0 +1,22 @@
+// //Void
+// function print(): void{
+//     console.log("Apni Coding");
+//     // return "Apni Coding"
+// };
+// const result = print();
+// console.log(result)
+// // let nothing : void = undefined;
+// // nothing = "vivek"
+//Never
+function loopFunction() {
+    while (true) {
+        console.log("Apni Coding");
+    }
+}
+function throwError() {
+    throw new Error("Throw an error");
+}
+function add() {
+    console.log(2 + 2);
+}
+// loopFunction()
