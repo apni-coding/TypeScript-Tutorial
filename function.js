c31bd7bb35e620ae9be5febf80cf4a49a82de3dd
apni-coding <apnicoding72@gmail.com>
2025-05-22 20:42:45 +0530

    function part 2

--- function.js
+++ function.js
@@ -2,12 +2,38 @@
 //     //logic
 // }
 // printValue()
-function sum(a, b) {
-    return Number(a) + b;
+// function sum(a:number|string, b:number):number|string{
+//     return Number(a)+b;
+// }
+// const result = sum(5, 10)
+// console.log(result)
+// function greetingUser(userName:string):void{
+//     console.log(`Welcome to apni coding ${userName}`)
+// }
+// greetingUser("Vivek")
+// function log(message:string, userId?: number){
+//     console.log(`${message} ${userId ? userId : ""}`)
+// }
+// log("Guest")
+// function greetUser(name:string = "Guest"){
+//     console.log(`welcome to apni coding ${name}`)
+// }
+// greetUser()
+//Arrow function
+// const test = (a:number, b:number):number=>{
+//     return a+b;
+// }
+//Rest Parameter
+function sum() {
+    var value = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        value[_i] = arguments[_i];
+    }
+    console.log(value);
+    var c = value.reduce(function (total, num) { return total + num; }, 0);
+    console.log(c);
+    return c;
 }
-var result = sum(5, 10);
-console.log(result);
-function greetingUser(userName) {
-    console.log("Welcome to apni coding ".concat(userName));
-}
-greetingUser("Vivek");
+sum(1, 2);
+sum(1, 2, 3);
+sum(1, 2, 3, 4);
