5566df2d9fc6019ab7a8ccc45a5daeae2ea1954d
apni-coding <apnicoding72@gmail.com>
2025-05-25 11:59:21 +0530

    array and function as a interface

--- interface.ts
+++ interface.ts
@@ -1,22 +1,45 @@
-interface UserInfo {
-  name: string;
-  age: number | string;
-  gender: string;
+// interface UserInfo {
+//   name: string;
+//   age: number | string;
+//   gender: string;
+// }
+
+// interface EmployeeInterface extends UserInfo {
+//   salary: number;
+// }
+
+// let studentInfo: UserInfo = {
+//   name: "Vivek",
+//   age: 20,
+//   gender: "Male",
+// };
+
+// let employeeInfo: EmployeeInterface = {
+//   name: "Vivek",
+//   age: 20,
+//   gender: "Male",
+//   salary: 25000,
+// };
+
+interface KeyValueProcessor {
+  (key: number, value: string): void;
 }
 
-interface EmployeeInterface extends UserInfo {
-  salary: number;
+let result: KeyValueProcessor = function (key: number, value: string): void {
+  console.log(key, value);
+};
+
+result(1, "abc");
+
+// function print(key:number, value: string):void{
+//     console.log(key, value)
+// }
+
+// print(1, "abc")
+
+
+interface NumStr{
+    [index:number]:(number|string|boolean)
 }
 
-let studentInfo: UserInfo = {
-  name: "Vivek",
-  age: 20,
-  gender: "Male",
-};
-
-let employeeInfo: EmployeeInterface = {
-  name: "Vivek",
-  age: 20,
-  gender: "Male",
-  salary: 25000,
-};
+let numArr:NumStr = [1, 2, 3, "one"];
