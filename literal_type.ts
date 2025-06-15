09a02989e8f9fd5fcc3fd5746a0b0e7a64073319
apni-coding <apnicoding72@gmail.com>
2025-06-15 12:48:05 +0530

    literal type in typescript

---
+++ literal_type.ts
@@ -0,0 +1,14 @@
+let direction : "left" | "up" | "down";
+
+direction = "left"
+direction = "up"
+// direction = "abc"
+
+let httpStatus : 200 | 404 | 500
+
+
+function move(direction: "left" | "up"){
+
+}
+
+move("left")
