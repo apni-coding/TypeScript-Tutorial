e43dbe3f496d4874ef28d93ae3eacbe244552967
apni-coding <apnicoding72@gmail.com>
2025-07-10 20:58:43 +0530

    type assertion in typescript

---
+++ typeAssetion.ts
@@ -0,0 +1,7 @@
+let someValue : any = "Hello, Welcome to apni coding";
+let strLength =  (someValue as string).length
+
+let xyz = (<string>someValue).length
+//.tsx
+
+console.log(someValue.length);
