6ea5ee98ec0cc2d00c8471c207699339814adf20
apni-coding <apnicoding72@gmail.com>
2025-07-18 10:39:12 +0530

    utility type - ii in typescript

--- utilityType.ts
+++ utilityType.ts
@@ -4,6 +4,41 @@ type User = {
   email?: string;
 };
 
+//Omit<T, K> -> removes one or more keys from a type
+
+type UserWithoutEmail = Omit<User, "email">
+
+//Exclude<T, U>   -> Removes types from a union that are assignable to another type.
+type Status = "success" | "error" | "loading"
+
+const withoutError : Exclude<Status, "error"> = "loading"
+
+
+//Extract<T, U> ---> Keeps only the types from a union that are assignable to another type
+
+const onlyError : Extract<Status, "error"> = "error"
+
+//NonNullable<T> --> Remove null & undefined from a type
+type MayBeString = string | null | undefined;
+
+type StrictString = NonNullable<MayBeString>
+
+//Record<K, T>  --> Constructs an object type with specified keys and value types
+type Roles  = "admin" | "user" | "guest"
+
+type RolePermissions = Record<Roles, boolean>
+
+// {
+//   admin:boolean,
+//   user: boolean,
+//   guest: boolean
+// }
+
+
+
+
+
+
 type UserPreview = Pick<User, "age" | "email">
 
 const userPreview : UserPreview = {
